9983aaefdb09ba7a7a8cecc9d04b9ab6d842c162
kmille <github@androidloves.me>
2023-06-19 10:26:01 +0200

    Fix unencrypted/chroot.sh

--- unencrypted/chroot.sh
+++ unencrypted/chroot.sh
@@ -4,8 +4,13 @@ set -euo pipefail
 if ! sudo losetup | grep -q $(pwd)/disk-unencrypted.img
 then
     sudo kpartx -a disk-unencrypted.img
-    LOOP_DEVICE=$(sudo losetup | grep $(pwd)/disk-unencrypted.img | awk '{ print $1 }' | awk -F/ '{ print $3 }')
     sudo losetup
+fi
+
+LOOP_DEVICE=$(sudo losetup | grep $(pwd)/disk-unencrypted.img | awk '{ print $1 }' | awk -F/ '{ print $3 }')
+
+if ! mountpoint -q /dev/mapper/"${LOOP_DEVICE}"p1
+then
     sudo mount /dev/mapper/"${LOOP_DEVICE}"p1 /mnt
 fi
 
